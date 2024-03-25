cd60dc7c90ce1b24b8f7b411c3065b400509f9e9
Abel Llorente <86959790+Abelarizaa@users.noreply.github.com>
2024-03-25 10:54:20 +0100

    Pasando valores anotacióin @Value
    Y creación de varios archivos properties

--- src/main/java/com/abel/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/abel/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,13 +1,21 @@
 package com.abel.curso.springboot.webapp.springbootweb.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import com.abel.curso.springboot.webapp.springbootweb.dto.ParamDto;
+import com.abel.curso.springboot.webapp.springbootweb.models.User;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
@@ -15,6 +23,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+   // @Value("${config.message}")
+   // private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz (@PathVariable String message){
@@ -35,5 +55,26 @@ public class PathVariableController {
 
         return json;
     }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        //Hacer algo con el usuario save en la bbdd
+        user.setName(user.getName().toUpperCase());
+        
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String,Object> values(@Value("${config.message}") String message) {
+       
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return json;
+    }
+    
+    
     
 }
